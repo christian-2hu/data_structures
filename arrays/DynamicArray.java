2378f364e9b10c6703d204c67543ece46a2e6de4
CHR <60179743+vanilla03@users.noreply.github.com>
2022-11-19 18:42:19 -0300

    Add constructors and generics

--- arrays/DynamicArray.java
+++ arrays/DynamicArray.java
@@ -1,5 +1,22 @@
-class DynamicArray {
-    public static void main(String[] args) {
-        
+package arrays;
+
+public class DynamicArray {
+    private int capacity;
+    private int size = 0;
+    private Object[] arr;
+
+    /*
+     * If no arguments are provided, then the capacity is 4 by default
+     */
+    public DynamicArray() {
+        this.capacity = 4;
+        this.arr = new Object[this.capacity];
+    }
+    /*
+     * If there's an argumento for the constructor, then use it as the capacity
+     */
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.arr = new Object[this.capacity];
     }
 }
