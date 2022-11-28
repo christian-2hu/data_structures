e76e142c53a60be5766510217bb2bd859d14ba08
CHR <60179743+vanilla03@users.noreply.github.com>
2022-11-28 01:20:13 -0300

    Tests on main

--- Queue/Main.java
+++ Queue/Main.java
@@ -1,5 +1,17 @@
 package Queue;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Queue<String> queue = new Queue<>();
+        System.out.println(queue.isEmpty());
+        queue.enqueue("First request");
+        queue.enqueue("Second request");
+        queue.enqueue("Random value");
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+
+    }
 }
