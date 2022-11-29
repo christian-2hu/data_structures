0b7c57a7d9d1406bc64e07dc050f0df70da74d14
CHR <60179743+vanilla03@users.noreply.github.com>
2022-11-29 11:28:49 -0300

    Start doubly linked list implementation
    A linked list with a previous and a next pointer

---
+++ LinkedList/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,12 @@
+package LinkedList.DoublyLinkedList;
+
+public class DoublyLinkedList {
+    DoublyLinkedList(){}
+
+}
+
+class Node {
+    protected int data;
+    protected Node next;
+    protected Node previous;
+}
---
+++ LinkedList/DoublyLinkedList/Main.java
@@ -0,0 +1,7 @@
+package LinkedList.DoublyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
