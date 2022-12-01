a53611b5499de3701b224a3f2b62b361eb017a5c
CHR <60179743+vanilla03@users.noreply.github.com>
2022-12-01 08:52:05 -0300

    Start stack implementation

--- Stack/README.md
+++ Stack/README.md
@@ -3,6 +3,6 @@
 - [ ] push(value) - adds value at position at tail
 - [ ] pop() - returns value and removes least recently added element (front)
 - [ ] isEmpty()
-- [ ] isFull()
+- [ ] isFull() - only if using arrays 
 - [ ] peek() returns the object at the top of the stack without removing it from the stack or modifying the stack
 - [ ] size()
---
+++ Stack/Stack.java
@@ -0,0 +1,31 @@
+package Stack;
+
+public class Stack<T> {
+    private int size = 0;
+    Stack(){}
+
+    public void push(T value) {
+
+    }
+    public T pop() {
+
+    }
+    public boolean isEmpty() {
+
+    }
+    public T peek() {
+
+    }
+    public int size() {
+
+    }
+}
+
+class Node<T> {
+    protected T data;
+    protected Node<T> next;
+
+    Node(T data) {
+        this.data = data;
+    }
+}
