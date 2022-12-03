1eb8d43af264606981263a7e45614f27c74992e2
CHR <60179743+vanilla03@users.noreply.github.com>
2022-12-03 14:30:06 -0300

    Implement hashtable's constructor

--- HashTable/HashTable.java
+++ HashTable/HashTable.java
@@ -1,5 +1,25 @@
 package HashTable;
 
 public class HashTable<K, V> {
+    private Object[] key;
+    private Object[] value;
+    private int capacity;
+    private int size = 0;
 
+    /*
+     * If no arguments are provided, then the capacity is 4 by default
+     */
+    public HashTable() {
+        this.capacity = 8;
+        this.key = new Object[this.capacity];
+        this.value = new Object[this.capacity];
+    }
+    /*
+     * If there's an argument for the constructor, then use it as the capacity
+     */
+    public HashTable(int initialCapacity) {
+        this.capacity = initialCapacity;
+        this.key = new Object[this.capacity];
+        this.value = new Object[this.capacity];
+    }
 }
--- HashTable/README.md
+++ HashTable/README.md
@@ -2,7 +2,7 @@
 
  Implement with array using linear probing
 
-- [ ] hash(k, m) - m is size of hash table
+- [x] hash(k, m) - m is size of hash table
 - [ ] add(key, value) - if key already exists, update value
 - [ ] exists(key)
 - [ ] get(key)
